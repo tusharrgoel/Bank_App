880818d3beb6ef4f794ac232be19a704fcd93795
tushargoell <tushar.goel@univolenitsolutions.com>
2024-01-03 15:56:29 +0530

    feat: added_card_deal

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,7 @@
 const Button = ({styles}) => {
   return (
     <div>
-      <button type= "button" className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary outline-none ${styles}`}>
+      <button type= "button" className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] rounded-[10px] text-primary outline-none ${styles}`}>
       Get Started
       </button>
     </div>
